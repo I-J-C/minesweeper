13e82f9d1d982848014f9d34dc115c44d92230dc
Ilka Collado <student07@Ilka-Collado.home>
2022-03-28 16:49:04 -0400

    working mine tile selector and surrounding tile counters, not connected to corresponding elements yet

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 class Tile {
-    constructor(grid, [row, column], mine = false) {
+    constructor(grid, [row, column]) {
         let id = `${row}-${column}`;
-        this.mine = mine;
+        this.mine = false;
         this.counter = null;
         this.row = row;
         this.column = column;
@@ -11,10 +11,10 @@ class Tile {
     }
 
     createTileElement() {
-        let newElement =  document.createElement('div');
+        let newElement = document.createElement('div');
         newElement.setAttribute('id', this.id);
         newElement.classList.add('tile');
-        console.log(newElement);
+        // console.log(newElement);
         return newElement;
     }
 
@@ -34,7 +34,7 @@ class Tile {
             let matrix = this.matrixFormation();
             for (let i = 0; i < matrix.length; i++) {
                 for (let j = 0; j < matrix[i].length; j++) {
-                    if (matrix[i][j].mine === false) {
+                    if (matrix[i][j] !== undefined && matrix[i][j].mine === false) {
                         matrix[i][j].countUp();
                     }
                 }
@@ -44,46 +44,93 @@ class Tile {
 
 
     matrixFormation() {
-        //form empty matrix
-        let matrix3 = Array(3).fill().map(() => (Array(3)));
+        //form populated matrix
+        let matrix = []
         for (let i = -1; i <= 1; i++) {
+            matrix[i + 1] = [];
             for (let j = -1; j <= 1; j++) {
-                matrix3[i + 1][j + 1] = this.grid[this.row + i][this.column + j];
+                if (this.grid[this.row + i] !== undefined && this.grid[this.column + j] !== undefined) {
+                    matrix[i + 1][j + 1] = this.grid[this.row + i][this.column + j];
+                    //mark center tile somehow?
+                }
             }
         }
-        return matrix3;
+        console.log(matrix);
+        return matrix;
     }
+
+    //Insert tile reveal function here
+
+    //Insert recursive blank opening here
 }
 
 //class for the grid formation upon game start
-class Grid {
+class Board {
     constructor(rows, columns, mineCount = 10) {
         this.rows = rows;
         this.columns = columns;
         this.mineCount = mineCount;
+        this.tiles = [];
+        this.grid = [];
         this.makeGrid();
+        this.makeTiles();
+        this.chooseMines();
+        // this.grid = [];
     }
 
     makeGrid() {
-        this.grid = Array(this.rows).fill().map(()=>(Array(this.columns).fill()));
+        let newGrid = []
+        let counter = 0;
+        for (let i = 0; i < this.rows; i++) {
+            newGrid[i] = [];
+            for (let j = 0; j < this.columns; j++) {
+                newGrid[i][j] = counter;
+                counter++
+            }
+        }
+        this.grid = newGrid;
+
+
+    }
+
+
+    makeTiles() {
+        //creating elements for the grid
         let gridElement = document.getElementById('grid');
-        for (let i=0; i<this.rows; i++) {
+        for (let i = 0; i < this.rows; i++) {
             let newRow = document.createElement('div');
             newRow.classList.add('row');
             newRow.setAttribute('id', `row${i}`);
             gridElement.appendChild(newRow);
             // console.log(newRow);
-            for (let j=0; j<this.columns; j++) {
-                let newTile = new Tile (this.grid, [i, j]);
+            for (let j = 0; j < this.columns; j++) {
+                let newTile = new Tile(this.grid, [i, j]);
+                this.grid[i][j] = newTile;
                 newRow.appendChild(newTile.createTileElement());
-                // console.log(newTile.element);
+                this.tiles.push(newTile);
             }
         }
+        // console.log(this.grid);
+    }
+
+    chooseMines() {
+        let mineArray = [];
+        while (mineArray.length < this.mineCount) {
+            let mineNumber = Math.floor(Math.random() * (this.rows * this.columns));
+            if (!mineArray.includes(mineNumber)) {
+                mineArray.push(mineNumber);
+                let mineTile = this.tiles[mineNumber];
+                mineTile.mine = true;
+                // console.log(mineNumber);
+                mineTile.mineCounter();
+            }
+        }
+        // console.log(mineArray);
     }
 }
 //the grid is in this format: grid[row][column];
 // grid = Array(10).fill().map(() => (Array(10).fill('tile')));
-let grid = new Grid(10, 10).grid;
+let grid = new Board(10, 10).grid;
 // console.log(grid);
 // let tile = new Tile(grid, [3, 3]);
 // grid[3][3] = tile;
