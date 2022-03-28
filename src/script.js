29fc7ecd071577325bbeb390e55a9a1d4bc86243
Ilka Collado <student07@Ilka-Collado.home>
2022-03-28 15:00:40 -0400

    added Grid class and method to populate html file with tiles, and accompanying basic css styling

--- src/script.js
+++ src/script.js
@@ -10,6 +10,14 @@ class Tile {
         this.element = document.getElementById(id);
     }
 
+    createTileElement() {
+        let newElement =  document.createElement('div');
+        newElement.setAttribute('id', this.id);
+        newElement.classList.add('tile');
+        console.log(newElement);
+        return newElement;
+    }
+
     //to tick up the counter for numbers on the grid
     countUp() {
         if (this.counter === null) {
@@ -17,7 +25,7 @@ class Tile {
         } else {
             this.counter++;
         }
-        console.log(this.counter);
+        // console.log(this.counter);
     }
 
     //this increments the counter for numbered tiles
@@ -38,21 +46,48 @@ class Tile {
     matrixFormation() {
         //form empty matrix
         let matrix3 = Array(3).fill().map(() => (Array(3)));
-        // console.log(matrix3);
         for (let i = -1; i <= 1; i++) {
             for (let j = -1; j <= 1; j++) {
                 matrix3[i + 1][j + 1] = this.grid[this.row + i][this.column + j];
             }
         }
-        console.log(matrix3);
         return matrix3;
     }
 }
+
+//class for the grid formation upon game start
+class Grid {
+    constructor(rows, columns, mineCount = 10) {
+        this.rows = rows;
+        this.columns = columns;
+        this.mineCount = mineCount;
+        this.makeGrid();
+    }
+
+    makeGrid() {
+        this.grid = Array(this.rows).fill().map(()=>(Array(this.columns).fill()));
+        let gridElement = document.getElementById('grid');
+        for (let i=0; i<this.rows; i++) {
+            let newRow = document.createElement('div');
+            newRow.classList.add('row');
+            newRow.setAttribute('id', `row${i}`);
+            gridElement.appendChild(newRow);
+            // console.log(newRow);
+            for (let j=0; j<this.columns; j++) {
+                let newTile = new Tile (this.grid, [i, j]);
+                newRow.appendChild(newTile.createTileElement());
+                // console.log(newTile.element);
+            }
+        }
+    }
+}
 //the grid is in this format: grid[row][column];
-grid = Array(10).fill().map(() => (Array(10).fill('tile')));
-console.log(grid);
-let tile = new Tile(grid, [3, 3]);
-grid[3][3] = tile;
+// grid = Array(10).fill().map(() => (Array(10).fill('tile')));
+let grid = new Grid(10, 10).grid;
+// console.log(grid);
+// let tile = new Tile(grid, [3, 3]);
+// grid[3][3] = tile;
+
 
 // tile.countUp();
 // tile.countUp();
